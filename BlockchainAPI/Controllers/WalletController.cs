93ecc6e4c76f505677cc9429a24858f8ff0174ba
KodYazıyorum59 <efehuner42@gmail.com>
2026-04-30 15:45:01 +0300

    İlk cüzdan API uç noktası eklendi ve sahte veriler bağlandı

---
+++ BlockchainAPI/Controllers/WalletController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlockchainAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDummyWallets()
+        {
+            var dummyWallets = new[]
+            {
+                new { Id = 1, Address = "0xA1B2C3...", Balance = 150.5 },
+                new { Id = 2, Address = "0x9F8E7D...", Balance = 42.0 },
+                new { Id = 3, Address = "0x112233...", Balance = 0.5 }
+            };
+
+            return Ok(dummyWallets);
+        }
+    }
+}
