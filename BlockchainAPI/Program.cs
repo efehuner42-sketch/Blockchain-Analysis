6fd9fe1c9c33daa55524c615f6950d5ceef490ce
KodYazıyorum59 <efehuner42@gmail.com>
2026-05-30 02:49:30 +0300

    Frontend ve Backend Cytoscape veri uyumsuzluğu giderildi, dropdown hatası çözüldü.

--- BlockchainAPI/Program.cs
+++ BlockchainAPI/Program.cs
@@ -1,10 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
+// 1. CORS Ayarlarını Servislere Ekle (Herkese açık hale getiriyoruz)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()   // Herhangi bir adresten (file:// dahil) gelen isteğe izin ver
+              .AllowAnyMethod()   // GET, POST gibi tüm işlemlere izin ver
+              .AllowAnyHeader();  // Tüm veri başlıklarına izin ver
+    });
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// 2. CORS Politikasını Uygulamaya Tanıt (Burası çok önemli, MapControllers'dan önce olmalı)
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
